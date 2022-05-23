01fbda4f1462d33ea8628d07cba8a9772503d472
Ultranaco <ultranaco@outlook.com>
2022-05-23 02:59:05 -0500

    feat: delete dependency for Newtonsoft and upgrade to net 6

--- SerializeExtend.cs
+++ SerializeExtend.cs
@@ -1,9 +1,8 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Ultranaco.Objects
@@ -11,38 +10,21 @@ namespace Ultranaco.Objects
    public enum CaseSensitive
    {
       None,
-      CamelLowerCase
-   }
+      UpperCamelCase,
+      LowerCaemlCase,
+      SnakeCase
+   } // TODO: implement case flavor
 
    public static class SerializeExtend
    {
       public static string SerializeJson(this object obj)
       {
-         return obj.SerializeJson(Formatting.None, CaseSensitive.CamelLowerCase);
-      }
-
-      public static string SerializeJson(this object obj, Formatting formating)
-      {
-         return obj.SerializeJson(formating, CaseSensitive.None);
-      }
-
-      public static string SerializeJson(this object obj, CaseSensitive caseSensitive)
-      {
-         return obj.SerializeJson(Formatting.None, caseSensitive);
-      }
-
-      public static string SerializeJson(this object obj, Formatting formating, CaseSensitive caseSensitive)
-      {
-         if (caseSensitive == CaseSensitive.None)
-            return JsonConvert.SerializeObject(obj);
-         var jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-         return JsonConvert.SerializeObject(obj, formating, jsonSerializerSettings);
-
+         return JsonSerializer.Serialize(obj);
       }
 
       public static T DeserializeJson<T>(this string obj)
       {
-         return JsonConvert.DeserializeObject<T>(obj);
+         return JsonSerializer.Deserialize<T>(obj);
       }
    }
 }
